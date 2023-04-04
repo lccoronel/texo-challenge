79eecda30882e4e549a562ceef4890c9f27ef933
Lucas Coronel <lccoronel7@icloud.com>
2023-04-04 16:51:48 -0300

    feat: :art: enviroment variables
    Setup dotenv lib to work with .env file and zod lib to validate the variables

---
+++ backend/src/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+    NODE_ENV: z.enum(['dev']).default('dev'),
+    PORT: z.coerce.number().default(3333)
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+    const envError = 'Invalid enviroment variables'
+
+    console.log(envError, _env.error.format())
+
+    throw new Error(envError)
+}
+
+export const env = _env.data
