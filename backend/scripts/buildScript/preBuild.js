896ab2a97e3fdbb39de5dbfee918ec41e04fa25c
Lucas Coronel <lccoronel7@icloud.com>
2023-04-27 18:57:07 -0300

    fix: build scripts

--- backend/scripts/buildScript/preBuild.js
+++ backend/scripts/buildScript/preBuild.js
@@ -1,6 +1,6 @@
 const moveFile = require('./helper/movefile')
 
 const CSVFile = './src/seed/uploadMovies/data/movielist.csv'
-const destination = './scripts/build/data'
+const destination = './scripts/buildScript/data'
 
 moveFile(CSVFile, destination)
