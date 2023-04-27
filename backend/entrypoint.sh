efe7ef0914163bb6b6d09e9b26c97a2fd74709ba
Lucas Coronel <lccoronel7@icloud.com>
2023-04-27 18:18:50 -0300

    wip: entrypoint file

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-if [[ $NODE_ENV = "development" ]] ; then
-    npx prisma migrate dev 
-    npm run seed
-    npm run start:dev
-else 
+# if [[ $NODE_ENV = "development" ]] ; then
+#     npx prisma migrate dev 
+#     npm run seed
+#     npm run start:dev
+# else 
     npm run build
     npx prisma migrate deploy 
     npm run seed:prod
     npm run start:prod
-fi
+# fi
