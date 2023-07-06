c38e1917193996accf3a6fe574ed2662eceab14c
Lucas Coronel <lccoronel7@icloud.com>
2023-07-06 09:46:56 -0300

    feat: CT test

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,4 +1,4 @@
 import { app } from './app'
 import { env } from '@/shared/infra/env'
 
-app.listen(3333, () => console.log(`Server is running on ${env.PORT} 🚀`))
+app.listen(3333, () => console.log(`Server is running on ${env.PORT} 🚀🚀`))
